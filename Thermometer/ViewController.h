d65fc96dd32009fe964e95e24f82ddabd82cb19f
Realank <realank@adsl-172-10-2-230.dsl.sndg02.sbcglobal.net>
2016-07-12 15:53:00 +0800

    Initial Commit

---
+++ Thermometer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Thermometer
+//
+//  Created by Realank on 16/7/12.
+//  Copyright © 2016年 Realank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
